1690ea3dbaccaf5c53a8eb35250d936a92c7c3c3
soumia <soumia.sahtani@gmail.com>
2023-10-30 23:44:28 +0100

    add file

---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./*.html"],
+//   theme: {
+//     fontFamily: {
+//       'Saira': ['Saira Condensed', 'sans-serif']
+//     },
+//     extend: {
+//       colors: {
+//         'background': '#000614',
+//         'secondary': '#030029',
+//         'blueText': '#0036AC',
+//         'primary': '#2E4BFF',
+//         'dark': '#080E1B',
+//         'blueText2': '#0E41F3',
+//       },
+//     },
+//   },
+//   plugins: [],
+// }
+// index.mjs
+import { Config } from 'tailwindcss';
+
+const config = {
+  content: ["./*.html"],
+  theme: {
+    fontFamily: {
+      'Saira': ['Saira Condensed', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        'background': '#000614',
+        'secondary': '#030029',
+        'blueText': '#0036AC',
+        'primary': '#2E4BFF',
+        'dark': '#080E1B',
+        'blueText2': '#0E41F3',
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
