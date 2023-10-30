302442f5b9980f4c12287a4a5b6c0f32e3fdb551
SoufianeAbo <abounasrsoufiane@gmail.com>
2023-10-30 18:17:06 +0100

    add js, finish navbar somewhat

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    fontFamily: {
+      'Saira': ['Saira Condensed', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        'background': '#000614',
+        'secondary': '#030029',
+        'blueText': '#0036AC',
+        'primary': '#2E4BFF',
+        'dark': '#080E1B',
+        'blueText2': '#0E41F3',
+      },
+    },
   },
   plugins: [],
 }
