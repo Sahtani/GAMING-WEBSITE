af20a07a4694ecf4af7c5c5a7a2290379f21e085
MohammedFytahi <mohammedfytahi567@gmail.com>
2023-11-02 09:11:50 +0100

    page home

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    fontFamily: {
+      'Saira': ['Saira Condensed', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        'background': '#000614',
+        'secondary': '#030029',
+        'blueText': '#0036AC',
+        'primary': '#2E4BFF',
+        'dark': '#080E1B',
+        'blueText2': '#0E41F3',
+        'customize': '#17284C',
+        'checkout': '#1E3B79',
+        'check': '#D44F24',
+      },
+    },
+  },
+  plugins: [],
+}
+
